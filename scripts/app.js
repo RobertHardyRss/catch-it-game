105b2f784fe3c95fddb026b2b65e2db3806f457d
Robert Hardy <rhardy@dcsdk12.org>
2021-09-09 17:11:33 -0600

    Create player object and keydown event listener

--- scripts/app.js
+++ scripts/app.js
@@ -6,10 +6,33 @@ canvas.height = 600;
 
 let goodBlockRatio = 0.25;
 let blockSpawnRate = 500;
+const BLOCK_SIZE = 32;
+
+
+
+
+let player = {
+    x: 0,
+    y: canvas.height - BLOCK_SIZE * 3,
+    width: BLOCK_SIZE * 2,
+    height: BLOCK_SIZE / 2,
+    update: function() {
+
+    },
+    render: function() {
+
+    }
+};
+
+window.addEventListener('keydown', (e) => {
+    console.log(e.key);
+});
+
+
 
 class Block {
     constructor() {
-        this.width = 32;
+        this.width = BLOCK_SIZE;
         this.height = this.width;
         this.x = Math.random() * canvas.width - this.width;
         this.y = 0 - this.height;  // off screen to start
@@ -52,6 +75,7 @@ function gameLoop(timestamp) {
 
     timeSinceLastBlock += changeInTime;
     if(timeSinceLastBlock >= blockSpawnRate) {
+        timeSinceLastBlock = 0;
         blocks.push(new Block());
     }
     
